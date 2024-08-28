5892f431638cd73dd48696744ea8bd1d7dae915d
yusuke.sugahara <yusuke.sugahara@gmail.com>
2024-08-28 09:53:10 +0900

    デプロイのための記述を追加

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path'); // pathモジュールをインポート
 require('dotenv').config(); // dotenvを使用して.envファイルを読み込む
 
 const app = express();
@@ -9,7 +10,6 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
-
 
 // MongoDBの接続
 mongoose.connect(process.env.MONGODB_URI, {
@@ -20,13 +20,19 @@ mongoose.connect(process.env.MONGODB_URI, {
     console.log('MongoDB connected');
   })
   .catch((err) => {
-    console.log("MONGODB_URI: ", process.env.MONGODB_URI);
     console.error('MongoDB connection error:', err);
   });
 
 const connection = mongoose.connection;
 connection.once('open', () => {
     console.log('MongoDB database connection established successfully');
+});
+
+// フロントエンドの静的ファイルを提供
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
 app.listen(PORT, () => {
