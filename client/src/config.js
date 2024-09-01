b56e3fa44b2676b59d8c0f5f9ec84ccc2dfa52a6
yusuke.sugahara <yusuke.sugahara@gmail.com>
2024-09-01 09:35:41 +0900

    configファイルの変更、server側のpackage.jsonの変更

---
+++ client/src/config.js
@@ -0,0 +1,6 @@
+const API_BASE_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://mern-task-app-server.vercel.app//api"
+    : "http://localhost:5000/api";
+
+export default API_BASE_URL;
