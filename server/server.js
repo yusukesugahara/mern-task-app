1c8ff824e5bfacb52ebdbc8ca7cb0a87cfb28a6f
yusuke.sugahara <yusuke.sugahara@gmail.com>
2024-09-02 19:07:34 +0900

    ローカルにて、clientからの通信許可設定

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: 'https://mern-task-app-theta.vercel.app' // フロントエンドのURLを許可
+  origin: ['https://mern-task-app-theta.vercel.app', 'http://localhost:3000']
 }));
 app.use(bodyParser.json());
 
